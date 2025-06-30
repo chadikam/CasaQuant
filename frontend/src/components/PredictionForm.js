c5f5a57e5e123cce9dbc4fa6223eb293bf719df5
chadikam <scaevola459@gmail.com>
2025-06-30 15:59:27 +0200

    Update PredictionForm.js

--- frontend/src/components/PredictionForm.js
+++ frontend/src/components/PredictionForm.js
@@ -145,7 +145,7 @@ const PredictionForm = () => {
       setPredictionError(null);
       
       try {
-        const response = await fetch('http://localhost:8000/predict', {
+        const response = await fetch('https://casaquant.onrender.com/predict', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
